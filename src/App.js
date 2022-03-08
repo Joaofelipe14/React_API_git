f89c2b7fc15c9a30c593a42ff4fca9a4865a6134
JoaoMelo <joaomelo@inforgeneses.com.br>
2022-03-08 10:20:42 -0300

    COSUMINDO API DO GIT

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import './styles.css';
+import imagem from './git.png';
+
+
+function App() {
+  const [search, setSearch] = useState('');
+  const [userData, setUserData] = useState();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    let url = `https://api.github.com/users/${search}`;
+    fetch(url)
+      .then(res => res.json())
+      .then(userResponse => setUserData(userResponse));
+  }
+
+  const handleChange = (event) => {
+    setSearch(event.target.value);
+  }
+
+  return (
+    <div className="py-5 px-1 container">
+      <h1>Github profile</h1>
+
+      <form action="" onSubmit={handleSubmit}>
+        <div className="input-group">
+          <input type="text" required value={search} onChange={handleChange} />
+        </div>
+
+        <button>Search</button>
+      </form>
+
+      {!userData && (
+        <div className="github-user none">
+          <p>Nenhum usuário encontrado. &#128531;</p>
+        </div>
+      )}
+
+      {userData && (
+        <div className="github-user">
+          <img src={userData.avatar_url} alt="Github Brand" />
+          <a href={userData.url}>{userData.name}</a>
+
+          <div className="info">
+            <p>{userData.location}</p>
+            <p>
+              <a href={userData.html_url} target="_new">{userData.html_url}</a></p>
+          </div>
+        </div>
+      )}
+    </div >
+  );
+}
+
+export default App;
